d63023a8e9404d72e63af2a30b2c04eed0682522
Andrey Beletsky <ecne@vr2.net>
2013-04-24 21:58:08 +0400

    fixed STATIC_URL in template renderer

--- pywkpdf/template.py
+++ pywkpdf/template.py
@@ -10,27 +10,26 @@ from . import html_to_pdf, html_to_pdf_file
 def render_to_pdf(template_name, dictionary=None,
                   context_instance=None, convert_args=None):
     convert_args = convert_args or {}
-    new_dictionary = {
-        'STATIC_URL': settings.STATIC_URL,
-        'PROJECT_PATH': settings.PROJECT_PATH
-    }
-    new_dictionary.update(dictionary)
-    result = render_to_string(template_name, new_dictionary, context_instance)
 
-    return html_to_pdf(result, **convert_args)
+    html = _render_template(template_name, dictionary, context_instance)
+    return html_to_pdf(html, **convert_args)
 
 
 def render_to_file(template_name, file_name, dictionary=None,
                    context_instance=None, convert_args=None):
     convert_args = convert_args or {}
+
+    html = _render_template(template_name, dictionary, context_instance)
+    return html_to_pdf_file(html, file_name, **convert_args)
+
+
+def _render_template(template_name, dictionary, context_instance):
     new_dictionary = {
-        'STATIC_URL': settings.STATIC_URL,
+        'STATIC_URL': settings.STATIC_ROOT,
         'PROJECT_PATH': settings.PROJECT_PATH
     }
     new_dictionary.update(dictionary)
-    result = render_to_string(template_name, new_dictionary, context_instance)
-
-    return html_to_pdf_file(result, file_name, **convert_args)
+    return render_to_string(template_name, new_dictionary, context_instance)
 
 
 class PDFTemplate(Template):
