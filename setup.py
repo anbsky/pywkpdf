433ce4c8d6ab79ab0f772b1b73b8d0ab311b853b
Andrey Beletsky <ecne@vr2.net>
2013-04-24 21:31:45 +0400

    added binaries to setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='pywkpdf',
     version='0.1dev',
     packages=['pywkpdf',],
+    package_data={'pywkpdf': ['bin/wkhtmltopdf-linux', 'bin/wkhtmltopdf.app/*']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     # test_suite='nose2.collector.collector',
