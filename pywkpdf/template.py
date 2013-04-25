088a3cbcc57a345eeb9a6fa9cfa74cfd8e06b54a
Andrey Beletsky <ecne@vr2.net>
2013-04-25 20:06:49 +0400

    added render_to_response

--- pywkpdf/template.py
+++ pywkpdf/template.py
@@ -1,8 +1,11 @@
 # encoding=utf-8
 
-from django.template import Template, Context
+from os import path
+from django.http import HttpResponse
+from django.template import Template, Context, RequestContext
 from django.template.loader import select_template, get_template, render_to_string
 from django.conf import settings
+from wsgiref.util import FileWrapper
 
 from . import html_to_pdf, html_to_pdf_file
 
@@ -19,6 +22,24 @@ def render_to_file(template_name, file_name, dictionary=None,
     convert_args = convert_args or {}
     html = _render_template(template_name, dictionary, context_instance)
     return html_to_pdf_file(html, file_name, **convert_args)
+
+
+def render_to_response(request, template_name, file_name=None,
+                       dictionary=None, convert_args=None):
+    pdf_file = render_to_pdf(
+        template_name, dictionary, RequestContext(request), convert_args)
+    file_name = file_name or path.basename(pdf_file.name)
+
+    response = HttpResponse(FileWrapper(pdf_file), content_type='application/pdf')
+    if file_name:
+        response['Content-Disposition'] = 'attachment; filename={}.pdf'.format(
+            file_name)
+    else:
+        response['Content-Disposition'] = 'attachment'
+    response['Content-Length'] = pdf_file.tell()
+    pdf_file.seek(0)
+
+    return response
 
 
 def _render_template(template_name, dictionary, context_instance):
